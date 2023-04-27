a175c6a67aa01e428544ed003973e9d00fc09e63
Rohit-Kavitake <kavitakerohit2001@gmail.com>
2023-04-27 23:28:39 +0530

    added html and initial js

--- contracts/todo.sol
+++ contracts/todo.sol
@@ -3,5 +3,38 @@ pragma solidity ^0.5.0;
 contract TodoList {
     
     uint public taskCount = 0;
+    mapping (uint => Task) public tasks;
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
+
+    constructor() public {
+        createTask("Task ! : Checking the construtor");
+    }
+
+    function createTask(string memory _content)public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+        emit TaskCreated(taskCount, _content,false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
+  }
     
 }
